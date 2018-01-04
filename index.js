86a7bc1268a6fbf9a6d9284aaa2c56ae8f9df1f8
Abhinav Verma <abhicsmnnit@gmail.com>
2018-01-05 02:01:03 +0530

    Initial commit for a Node Web Server App with many features for my reference later on - Used: 1) Express for Web Server. 2) Express Middlewares for Logging, Putting your site on maintenance, and Serving static content from a public folder. 3) Handlebars (HBS) View Engine for rendering dynamic HTML. 4) HBS Partials and Partial Helpers for reusing HTML Content

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+const app = express();
+const PORT = 3000;
+
+app.set('view engine', 'hbs');
+
+// Middlewares (MWs)
+// Logger MW
+app.use((req, res, next) => {
+    const now = new Date().toString();
+    const logLine = `${now}: ${req.method} ${req.url}`;
+    
+    console.log(logLine);
+    fs.appendFile(`${__dirname}/logs/server.log`, logLine + '\n', (err) => {
+        if (err) {
+            console.log('Unable to write to server logs!');
+        }
+    });
+    
+    next();
+});
+
+// Maintenance MW -- Uncomment the code below to put the site on maintenance!
+// app.use((req, res, next) => {
+//     res.render('maintenance'); 
+// });
+
+// Static MW
+app.use(express.static(`${__dirname}/public`));
+
+hbs.registerPartials(`${__dirname}/views/partials`);
+hbs.registerHelper('getCurrentYear', () => new Date().getFullYear());
+hbs.registerHelper('screamIt', (text) => text.toUpperCase());
+
+app.get('/', (req, res) => {
+    res.render('home', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Hare Krishna!'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        pageTitle: 'About Us!'
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Node web server listening at ${PORT}!`);
+});
